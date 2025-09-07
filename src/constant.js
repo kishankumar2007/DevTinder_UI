b66fe3a8bb96d5416c6d007e9508de741ca0cff0
mrperfectVip <kk7012234@gmail.com>
2025-09-07 19:49:58 +0530

    Update BASE_URL to production environment; switch from localhost to live server URL

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "https://devtinder-production-d1c4.up.railway.app"
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://devtinder-production-d1c4.up.railway.app"
+// const BASE_URL = "http://localhost:3000"
 export default BASE_URL
